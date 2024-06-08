6ea8432d7254730dc5ba2cb17852d48127d40a3b
Miguel dos Santos Palhares <79547108+Tcm23a@users.noreply.github.com>
2024-06-08 20:18:53 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
-console.log("Não esqueci de linkar com o HTML ;D")
+//parts do login ;D
+
+const userName= document.querySelector('.userName');
+const loginForm= login.querySelector('login__form');
+const loginButton= login.querySelector('loginButton');
+
+
+const user= {id: '', name: '', color:''};
+
+const handleSubmit= (event) =>{
+    event.preventDefault();
+    console.log(user);
+}
+
+loginForm.addEventListener('submit', handleSubmit);
