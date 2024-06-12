dd2d340ce6c90df8abec8519ebf1969e5fe0382e
Miguel dos Santos Palhares <79547108+Tcm23a@users.noreply.github.com>
2024-06-12 02:23:37 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,49 @@
 //parts do login ;D
+console.log('hello world')
 
-const userName= document.querySelector('.userName');
-const loginForm= login.querySelector('login__form');
-const loginButton= login.querySelector('loginButton');
+const login = document.querySelector(".login");
+const loginForm = login.querySelector("#userNameInput");
+const loginButton = login.querySelector("#loginButton");
+
+//parte do chat
+
+const chat= document.querySelector('.chat')
 
 
-const user= {id: '', name: '', color:''};
 
-const handleSubmit= (event) =>{
-    event.preventDefault();
-    console.log(user);
+const colors= [
+    'OrangeRed',
+    'Gold',
+    'Violet',
+    'Lime',
+    'Aqua',
+    'Pink'
+]
+
+const getRadomColor= () =>{
+    const radomIndex= Math.floor(Math.random() * colors.length);
+    return colors[radomIndex]
+
 }
 
-loginForm.addEventListener('submit', handleSubmit);
+const user= {id: '', name: '', color:''}
+
+
+const handleSubmit= (event) =>{
+    event.preventDefault()
+
+    user.id= crypto.randomUUID();
+    user.name= loginButton.value;
+    user.color= getRadomColor();
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    console.log(user);
+
+}
+
+loginForm.addEventListener("submit", handleSubmit);
+
+
+
