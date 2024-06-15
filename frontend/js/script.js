3071eb1f67f9a049dd1530340177394f5aad65df
Miguel dos Santos Palhares <79547108+Tcm23a@users.noreply.github.com>
2024-06-15 01:58:15 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -51,7 +51,7 @@ const handleLogin= (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("https://real-time-chat-zsno.onrender.com")
     websocket.onmessage = processMessage
 
 }
